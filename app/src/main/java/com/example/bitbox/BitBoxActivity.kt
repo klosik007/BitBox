b3233ca5e59725face423e95bcaac298124d0ff2
klosik007 <pklos1992@gmail.com>
2021-06-23 23:26:21 +0200

    SingleFragmentActivity() inheritance deleted

--- app/src/main/java/com/example/bitbox/BitBoxActivity.kt
+++ app/src/main/java/com/example/bitbox/BitBoxActivity.kt
@@ -1,9 +1,16 @@
 package com.example.bitbox
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class BitBoxActivity : SingleFragmentActivity(){
-    override fun createFragment(): Fragment {
-        return BitBoxFragment.newInstance()
+class BitBoxActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_container)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, BitBoxFragment())
+            .commit()
     }
 }
