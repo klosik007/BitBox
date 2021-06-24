3e27be052aedc25b4b3e1aca5e9cc9e5685a7da4
klosik007 <pklos1992@gmail.com>
2021-06-24 21:10:39 +0200

    play music funcs added

--- app/src/main/java/com/example/bitbox/BitBox.kt
+++ app/src/main/java/com/example/bitbox/BitBox.kt
@@ -1,15 +1,20 @@
 package com.example.bitbox
 
 import android.content.Context
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
 import java.io.IOException
 
 class BitBox(context: Context) {
     private val TAG: String = "BitBox"
     private val SOUNDS_FOLDER = "sample_sounds"
+    private val MAX_SOUNDS = 5
     private var assets: AssetManager = context.assets
     val sounds = arrayListOf<Sound>()
+    var soundPool: SoundPool.Builder = SoundPool.Builder()
+    var soundPoolInit = soundPool.build()
 
     init {
         loadSounds()
@@ -26,9 +31,29 @@ class BitBox(context: Context) {
         }
 
         for(soundName in soundNames){
-            val assetPath = "$SOUNDS_FOLDER/$soundName"
-            val sound = Sound(assetPath)
-            sounds.add(sound)
+            try{
+                val assetPath = "$SOUNDS_FOLDER/$soundName"
+                val sound = Sound(assetPath)
+                load(sound)
+                sounds.add(sound)
+            }catch (e: IOException){
+                Log.e(TAG, "Cannot load file ${soundName}", e)
+            }
         }
+    }
+
+    private fun load(sound: Sound){
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId: Int = soundPoolInit.load(afd, 1)
+        sound.soundId = soundId
+    }
+
+    fun play(sound: Sound){
+        val soundId: Int? = sound.soundId
+
+        if (soundId != null) {
+            soundPoolInit.play(soundId, 1.0f, 1.0f, 0, 1, 1.0f)
+        }
+
     }
 }
