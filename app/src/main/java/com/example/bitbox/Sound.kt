ec30c1a20010de68557fda9a8c416018ad123594
klosik007 <pklos1992@gmail.com>
2021-06-24 21:10:17 +0200

    soundId property added

--- app/src/main/java/com/example/bitbox/Sound.kt
+++ app/src/main/java/com/example/bitbox/Sound.kt
@@ -1,11 +1,12 @@
 package com.example.bitbox
 
 class Sound(assetPath: String) {
-    private var _assetPath: String = assetPath
+    var assetPath: String = assetPath
     var name: String
+    var soundId: Int? = null
 
     init{
-        val components = _assetPath.split("/")
+        val components = assetPath.split("/")
         val fileName = components[components.size - 1]
         name = fileName.replace(".wav", "")
     }
