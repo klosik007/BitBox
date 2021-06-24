e7797d0076723805e0e8f0c0a70efb7b754c78b7
klosik007 <pklos1992@gmail.com>
2021-06-24 20:04:35 +0200

    binds and notifyChanges() added

--- app/src/main/java/com/example/bitbox/SoundViewModel.kt
+++ app/src/main/java/com/example/bitbox/SoundViewModel.kt
@@ -4,13 +4,20 @@ import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 
 class SoundViewModel(bitbox: BitBox): BaseObservable(){
-    private var _bitBox: BitBox = bitbox
-    var sound: Sound = Sound("")
-        set(value){
-            field = value
-            notifyChange()
-        }
+    private var sound: Sound? = null
 
     @Bindable
-    var title: String = sound.name
+    @JvmName("getTitle1")
+    fun getTitle(): String{
+        return sound!!.name
+    }
+
+    fun getSound(): Sound? {
+        return sound
+    }
+
+    fun setSound(sound: Sound?) {
+        this.sound = sound
+        notifyChange()
+    }
 }
